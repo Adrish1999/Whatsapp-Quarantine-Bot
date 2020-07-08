519ce804e3a6b095cf07b004a17c447d2692d36d
Adrish Bose <adrishbose198@gmail.com>
2020-07-08 22:31:22 +0530

    Up

--- bot_app/views.py
+++ bot_app/views.py
@@ -71,7 +71,7 @@ You can give me the following commands:
             #         to='whatsapp:+918981181552'
             #     )
 
-            print("Sent message is : ",message.sid)
+            #print("Sent message is : ",message.sid)
             responded = True
 
         elif incoming_msg == 'cat':
